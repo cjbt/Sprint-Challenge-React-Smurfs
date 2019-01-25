b706ccb85161e749debbae2daa8c575e58717fa8
cecil john tantay <cjbtantay@gmail.com>
2019-01-25 09:06:36 -0800

    add functionality to nav and finish mvp

--- village/src/components/Nav.js
+++ village/src/components/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav>
-      <h1>here</h1>
+      <NavLink to='/'>HOME</NavLink>
+      <NavLink to='/smurf-add'>ADD SMURF</NavLink>
     </nav>
   );
 };
