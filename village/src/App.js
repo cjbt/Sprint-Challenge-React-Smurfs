ee10fa37cf6950951cc2c7f2cea08827f35cc5d1
cecil john tantay <cjbtantay@gmail.com>
2019-01-25 08:35:54 -0800

    attempt refractor of code, brought back to original state and added axios get on cdm

--- village/src/App.js
+++ village/src/App.js
@@ -1,23 +1,63 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
+const url = 'http://localhost:3333/smurfs';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
+      // newSmurfs: {
+      //   name: '',
+      //   height: '',
+      //   age: ''
+      // }
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios
+      .get(url)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          smurfs: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  // submitHandler = () => {
+  //   axios
+  //     .post(url, this.state.newSmurfs)
+  //     .then(res => {
+  //       console.log(res.data);
+  //       this.setState({
+  //         smurfs: res.data
+  //       });
+  //     })
+  //     .catch(err => console.log(err));
+  // };
+
+  // handleChange = e => {
+  //   this.setState(prevState => ({
+  //     newSmurfs: { ...prevState.newSmurfs, [e.target.name]: e.target.value }
+  //   }));
+  // };
   render() {
     return (
-      <div className="App">
-        <SmurfForm />
+      <div className='App'>
+        <SmurfForm
+          handleChange={this.handleChange}
+          submitHandler={this.submitHandler}
+          newSmurfs={this.state.newSmurfs}
+        />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,6 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SmurfForm extends Component {
+// const SmurfForm = props => {
+//   const { submitHandler, handleChange } = props;
+//   const { name, age, height } = props.newSmurfs;
+//   return (
+//     <div className='SmurfForm'>
+//       <form onSubmit={submitHandler}>
+//         <input
+//           onChange={handleChange}
+//           placeholder='name'
+//           value={name}
+//           name='name'
+//         />
+//         <input
+//           onChange={handleChange}
+//           placeholder='age'
+//           value={age}
+//           name='age'
+//         />
+//         <input
+//           onChange={handleChange}
+//           placeholder='height'
+//           value={height}
+//           name='height'
+//         />
+//         <button type='submit'>Add to the village</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default SmurfForm;
+
+class SmurfForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -19,7 +51,7 @@ class SmurfForm extends Component {
       age: '',
       height: ''
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -27,27 +59,27 @@ class SmurfForm extends Component {
 
   render() {
     return (
-      <div className="SmurfForm">
+      <div className='SmurfForm'>
         <form onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
-            placeholder="name"
+            placeholder='name'
             value={this.state.name}
-            name="name"
+            name='name'
           />
           <input
             onChange={this.handleInputChange}
-            placeholder="age"
+            placeholder='age'
             value={this.state.age}
-            name="age"
+            name='age'
           />
           <input
             onChange={this.handleInputChange}
-            placeholder="height"
+            placeholder='height'
             value={this.state.height}
-            name="height"
+            name='height'
           />
-          <button type="submit">Add to the village</button>
+          <button type='submit'>Add to the village</button>
         </form>
       </div>
     );
